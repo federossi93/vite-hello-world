f8559bb6be26c84c55d685579ffde5e04e42bafc
federossi93 <rossifederico0993@gmail.com>
2022-11-09 15:35:16 +0100

    add jumbo

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import titolo from "./components/AppTitle.vue";
+import jumbo from "./components/Jumbotron.vue"
 
 </script>
 
 <template>
+  <jumbo/>
   <h1>ciao</h1>
+  
   <titolo/>
 </template>
 
