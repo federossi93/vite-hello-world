c6c835c1172cb28e49aaeb60f8996751c8111552
federossi93 <rossifederico0993@gmail.com>
2022-11-09 14:08:29 +0100

    add import

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import titolo from "./components/AppTitle.vue";
+
+</script>
+
+<template>
+  <h1>ciao</h1>
+  <titolo/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2>vite</h2>
+</template>
+
+<style scoped>
+
+</style>
